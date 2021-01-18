60edd8e87849bc704f0c0aeb77b026452491ab8c
FIftyPercent <gustavojuliao1999@gmail.com>
2021-01-18 14:17:36 -0300

    Adição API Kivy, Desenvolvento Interface Grafica, Adição de sistema de comunicação por socket

--- Client.py
+++ Client.py
@@ -1,12 +1,31 @@
 
+
+import socket #pacote comunicação
+from threading import Thread
+
+
 #InterFace Gráfica Kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import (
-    NumericProperty, ReferenceListProperty, ObjectProperty
-)
+from kivy.properties import (NumericProperty, ReferenceListProperty, ObjectProperty)
 from kivy.vector import Vector
 from kivy.clock import Clock
+
+class conection():
+	def __init__(self):
+		# Iniciando o TCP/IP socket
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+		# Conectando Socket
+		server_address = ('localhost', 8008)
+		self.sock.connect(server_address)
+	def msg_send(self,msg):
+		# Envia Mensagem
+		self.sock.send(bytes(msg,'utf_8'))
+	def msg_recv(self):
+		res = self.sock.recv(1024)
+		data = res.decode('utf_8')
+
 
 class LoginScreen(Widget):
     username = ObjectProperty(None)
--- Server.py
+++ Server.py
@@ -1,2 +1,104 @@
+#Database
+import sqlite3
+
+from datetime import datetime
+
+import socket
+from threading import Thread
+
+class ThreadedServer(Thread):
+    def __init__(self, host, port, timeout=60, debug=False):
+        self.host = host
+        self.port = port
+        self.timeout = timeout
+        self.debug = debug
+        Thread.__init__(self)
+
+    # Iniciando Thread Server
+    def run(self):
+        if self.debug:
+            print(datetime.now())
+            print('Iniciando Server...', '\n')
+
+        self.listen()
+
+    def listen(self):
+        # Criando Instancia para o Socket
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        # ligando o soquete ao host e porta
+        self.sock.bind((self.host, self.port))
+        if self.debug:
+            print(datetime.now())
+            print('SERVIDOR do Socket Conectado ao', self.host, self.port, '\n')
+
+        # start listening for a client
+        self.sock.listen(5)
+        if self.debug:
+            print(datetime.now())
+            print('SERVER Recebendo...', '\n')
+        while True:
+            # get the client object and address
+            client, address = self.sock.accept()
+
+            # Definir tempo limite
+            #client.settimeout(self.timeout)
+
+            if self.debug:
+                print(datetime.now())
+                print('CLIENTE conectado', client, '\n')
+
+            #
+            # iniciando um Threado para receber do cliente
+            Thread(target = self.listenToClient,args = (client,address)).start()
+
+
+    def listenToClient(self, client, address):
+        # Difinindo o tamanho do buffer (pode ser 2048 ou 4096 / potência de 2)
+        size = 1024
+        while True:
+            try:
+                # Tentando receber dados do Cliente
+                data = client.recv(size).decode('utf-8')
+                if data:
+                    #data = loads(data.rstrip('\0'))
+                    if self.debug:
+                        print(datetime.now())
+                        print('CLIENT Data Recebido', client)
+                        print('Data:')
+                        print(data)
+                        if(data) == "break":
+                            break
+                        #pprint(data, width=1)
+                        print('\n')
+                        client.send((data).encode('utf-8'))
+                        print("END")
+
+                    #send a response back to the client
+                    '''res = {
+                        'cmd': data['cmd'],
+                        'data': data['data']
+                    }'''
+
+                    #response = dumps(res)
+
+                else:
+                    raise socket.error('Erro Cliente Desconectado')
+
+
+
+            except:
+                if self.debug:
+                    print(datetime.now())
+                    print('CLIENT Desconectado:', client, '\n')
+                client.close()
+                return False
+
+
+if __name__ == "__main__":
+    #iniciando Server(ip,porta, tempo maximo resposta, debug)
+    ThreadedServer('127.0.0.1', 8008, timeout=86400, debug=True).start()
+
 
 
